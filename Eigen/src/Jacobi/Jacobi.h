09074bb5efc831e89045ec4550eb9fe989596f93
arthurfeeney <afeeney@trinity.edu>
2022-07-19 16:40:27 -0500

    fix jacobi rotation vectorized path

--- Eigen/src/Jacobi/Jacobi.h
+++ Eigen/src/Jacobi/Jacobi.h
@@ -455,7 +455,7 @@ EIGEN_DEVICE_FUNC
 void /*EIGEN_DONT_INLINE*/ apply_rotation_in_the_plane(DenseBase<VectorX>& xpr_x, DenseBase<VectorY>& xpr_y, const JacobiRotation<OtherScalar>& j)
 {
   typedef typename VectorX::Scalar Scalar;
-  const bool Vectorizable =    (int(VectorX::Flags) & int(VectorY::Flags) & PacketAccessBit)
+  const bool Vectorizable =    (int(evaluator<VectorX>::Flags) & int(evaluator<VectorY>::Flags) & PacketAccessBit)
                             && (int(packet_traits<Scalar>::size) == int(packet_traits<OtherScalar>::size));
 
   eigen_assert(xpr_x.size() == xpr_y.size());
