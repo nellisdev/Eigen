56c8fd9eb5ccc89b7e972820009a023a156edcba
Gabriele Buondonno <gabriele.buondonno@wandercraft.eu>
2022-10-13 00:26:18 +0200

    Update snippet

--- doc/examples/tut_arithmetic_dot_cross.cpp
+++ doc/examples/tut_arithmetic_dot_cross.cpp
@@ -10,4 +10,8 @@ int main()
   double dp = v.adjoint()*w; // automatic conversion of the inner product to a scalar
   std::cout << "Dot product via a matrix product: " << dp << std::endl;
   std::cout << "Cross product:\n" << v.cross(w) << std::endl;
+
+  Eigen::Vector2d v2(1,2);
+  Eigen::Vector2d w2(0,1);
+  std::cout << "2D cross product: " << v2.cross(w2) << std::endl;
 }
