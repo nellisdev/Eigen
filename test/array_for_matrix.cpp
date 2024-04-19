3cc05b7318dbc2f2ebd5c526279b14b08dd92594
Charlie Schlosser <cs.schlosser@gmail.com>
2024-04-19 13:24:20 -0400

    format

--- test/array_for_matrix.cpp
+++ test/array_for_matrix.cpp
@@ -297,17 +297,17 @@ void regrrssion_bug_1410() {
 }
 
 EIGEN_DECLARE_TEST(array_for_matrix) {
-  //for (int i = 0; i < g_repeat; i++) {
-  //  CALL_SUBTEST_1(array_for_matrix(Matrix<float, 1, 1>()));
-  //  CALL_SUBTEST_2(array_for_matrix(Matrix2f()));
-  //  CALL_SUBTEST_3(array_for_matrix(Matrix4d()));
-  //  CALL_SUBTEST_4(array_for_matrix(
-  //      MatrixXcf(internal::random<int>(1, EIGEN_TEST_MAX_SIZE), internal::random<int>(1, EIGEN_TEST_MAX_SIZE))));
-  //  CALL_SUBTEST_5(array_for_matrix(
-  //      MatrixXf(internal::random<int>(1, EIGEN_TEST_MAX_SIZE), internal::random<int>(1, EIGEN_TEST_MAX_SIZE))));
-  //  CALL_SUBTEST_6(array_for_matrix(
-  //      MatrixXi(internal::random<int>(1, EIGEN_TEST_MAX_SIZE), internal::random<int>(1, EIGEN_TEST_MAX_SIZE))));
-  //}
+  for (int i = 0; i < g_repeat; i++) {
+    CALL_SUBTEST_1(array_for_matrix(Matrix<float, 1, 1>()));
+    CALL_SUBTEST_2(array_for_matrix(Matrix2f()));
+    CALL_SUBTEST_3(array_for_matrix(Matrix4d()));
+    CALL_SUBTEST_4(array_for_matrix(
+        MatrixXcf(internal::random<int>(1, EIGEN_TEST_MAX_SIZE), internal::random<int>(1, EIGEN_TEST_MAX_SIZE))));
+    CALL_SUBTEST_5(array_for_matrix(
+        MatrixXf(internal::random<int>(1, EIGEN_TEST_MAX_SIZE), internal::random<int>(1, EIGEN_TEST_MAX_SIZE))));
+    CALL_SUBTEST_6(array_for_matrix(
+        MatrixXi(internal::random<int>(1, EIGEN_TEST_MAX_SIZE), internal::random<int>(1, EIGEN_TEST_MAX_SIZE))));
+  }
   for (int i = 0; i < g_repeat; i++) {
     CALL_SUBTEST_1(comparisons(Matrix<float, 1, 1>()));
     CALL_SUBTEST_2(comparisons(Matrix2f()));
@@ -317,33 +317,33 @@ EIGEN_DECLARE_TEST(array_for_matrix) {
     CALL_SUBTEST_6(comparisons(
         MatrixXi(internal::random<int>(1, EIGEN_TEST_MAX_SIZE), internal::random<int>(1, EIGEN_TEST_MAX_SIZE))));
   }
-  //for (int i = 0; i < g_repeat; i++) {
-  //  CALL_SUBTEST_1(cwise_min_max(Matrix<float, 1, 1>()));
-  //  CALL_SUBTEST_2(cwise_min_max(Matrix2f()));
-  //  CALL_SUBTEST_3(cwise_min_max(Matrix4d()));
-  //  CALL_SUBTEST_5(cwise_min_max(
-  //      MatrixXf(internal::random<int>(1, EIGEN_TEST_MAX_SIZE), internal::random<int>(1, EIGEN_TEST_MAX_SIZE))));
-  //  CALL_SUBTEST_6(cwise_min_max(
-  //      MatrixXi(internal::random<int>(1, EIGEN_TEST_MAX_SIZE), internal::random<int>(1, EIGEN_TEST_MAX_SIZE))));
-  //}
-  //for (int i = 0; i < g_repeat; i++) {
-  //  CALL_SUBTEST_1(lpNorm(Matrix<float, 1, 1>()));
-  //  CALL_SUBTEST_2(lpNorm(Vector2f()));
-  //  CALL_SUBTEST_7(lpNorm(Vector3d()));
-  //  CALL_SUBTEST_8(lpNorm(Vector4f()));
-  //  CALL_SUBTEST_5(lpNorm(VectorXf(internal::random<int>(1, EIGEN_TEST_MAX_SIZE))));
-  //  CALL_SUBTEST_4(lpNorm(VectorXcf(internal::random<int>(1, EIGEN_TEST_MAX_SIZE))));
-  //}
-  //CALL_SUBTEST_5(lpNorm(VectorXf(0)));
-  //CALL_SUBTEST_4(lpNorm(VectorXcf(0)));
-  //for (int i = 0; i < g_repeat; i++) {
-  //  CALL_SUBTEST_4(resize(
-  //      MatrixXcf(internal::random<int>(1, EIGEN_TEST_MAX_SIZE), internal::random<int>(1, EIGEN_TEST_MAX_SIZE))));
-  //  CALL_SUBTEST_5(
-  //      resize(MatrixXf(internal::random<int>(1, EIGEN_TEST_MAX_SIZE), internal::random<int>(1, EIGEN_TEST_MAX_SIZE))));
-  //  CALL_SUBTEST_6(
-  //      resize(MatrixXi(internal::random<int>(1, EIGEN_TEST_MAX_SIZE), internal::random<int>(1, EIGEN_TEST_MAX_SIZE))));
-  //}
-  //CALL_SUBTEST_6(regression_bug_654<0>());
-  //CALL_SUBTEST_6(regrrssion_bug_1410<0>());
+  for (int i = 0; i < g_repeat; i++) {
+    CALL_SUBTEST_1(cwise_min_max(Matrix<float, 1, 1>()));
+    CALL_SUBTEST_2(cwise_min_max(Matrix2f()));
+    CALL_SUBTEST_3(cwise_min_max(Matrix4d()));
+    CALL_SUBTEST_5(cwise_min_max(
+        MatrixXf(internal::random<int>(1, EIGEN_TEST_MAX_SIZE), internal::random<int>(1, EIGEN_TEST_MAX_SIZE))));
+    CALL_SUBTEST_6(cwise_min_max(
+        MatrixXi(internal::random<int>(1, EIGEN_TEST_MAX_SIZE), internal::random<int>(1, EIGEN_TEST_MAX_SIZE))));
+  }
+  for (int i = 0; i < g_repeat; i++) {
+    CALL_SUBTEST_1(lpNorm(Matrix<float, 1, 1>()));
+    CALL_SUBTEST_2(lpNorm(Vector2f()));
+    CALL_SUBTEST_7(lpNorm(Vector3d()));
+    CALL_SUBTEST_8(lpNorm(Vector4f()));
+    CALL_SUBTEST_5(lpNorm(VectorXf(internal::random<int>(1, EIGEN_TEST_MAX_SIZE))));
+    CALL_SUBTEST_4(lpNorm(VectorXcf(internal::random<int>(1, EIGEN_TEST_MAX_SIZE))));
+  }
+  CALL_SUBTEST_5(lpNorm(VectorXf(0)));
+  CALL_SUBTEST_4(lpNorm(VectorXcf(0)));
+  for (int i = 0; i < g_repeat; i++) {
+    CALL_SUBTEST_4(resize(
+        MatrixXcf(internal::random<int>(1, EIGEN_TEST_MAX_SIZE), internal::random<int>(1, EIGEN_TEST_MAX_SIZE))));
+    CALL_SUBTEST_5(
+        resize(MatrixXf(internal::random<int>(1, EIGEN_TEST_MAX_SIZE), internal::random<int>(1, EIGEN_TEST_MAX_SIZE))));
+    CALL_SUBTEST_6(
+        resize(MatrixXi(internal::random<int>(1, EIGEN_TEST_MAX_SIZE), internal::random<int>(1, EIGEN_TEST_MAX_SIZE))));
+  }
+  CALL_SUBTEST_6(regression_bug_654<0>());
+  CALL_SUBTEST_6(regrrssion_bug_1410<0>());
 }
